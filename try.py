d5c79171f25227a05975c18933478edb63f08c0d
Sevilay Erkan <sevilayerkan2@gmail.com>
2022-09-01 16:40:56 +0300

    Add new document branch

---
+++ try.py
@@ -0,0 +1,31 @@
+
+from datetime import datetime
+
+from rich import box
+from rich.align import Align
+from rich.console import Console, Group
+from rich.layout import Layout
+from rich.panel import Panel
+from rich.progress import Progress, SpinnerColumn, BarColumn, TextColumn
+from rich.syntax import Syntax
+from rich.table import Table
+from rich.text import Text
+
+console = Console()
+
+def make_layout() -> Layout:
+    """Define the layout."""
+    layout = Layout(name="root")
+
+    layout.split(
+        Layout(name="header", size=3),
+        Layout(name="main", ratio=1),
+        Layout(name="footer", size=7),
+    )
+    layout["main"].split_row(
+        Layout(name="side"),
+        Layout(name="body", ratio=2, minimum_size=60),
+    )
+    layout["side"].split(Layout(name="box1"), Layout(name="box2"))
+    return layout
+
