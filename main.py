dc9f198d235f2d094f6623145de37b7fd1a05e84
Sevilay Erkan <sevilayerkan2@gmail.com>
2022-09-02 17:51:33 +0300

    Add show_log()

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 #Import required libraries
 import os
+from pickle import TRUE
 import subprocess
 
 #Shows all containers with their states
-#Çalışan tüm containerları gösterir
+#Tüm containerları gösterir
 def show():
 	subprocess.call("docker ps -a", shell=True)
+
+#Shows logs given container
+#Log kayıtlarını gösterir
+def show_log():
+    print("Var olan tüm konteynırlar şu şekildedir: \n" , show() , "\n")
+    containerName = input("\nLog kaydını görmek istediğiniz konteynırın ID'sini girin: \n")
+    if (containerName==0) :
+        print("error message")
+    subprocess.run("docker logs -f --details " + containerName, shell=True)
+    #Possible bug: timeout
+
 
 
 #Shows runable functions
@@ -17,6 +29,7 @@ def showFunctions():
 
 	#Show all functions
     print("1- Tüm konteynırları listele\n")
+    print("2- Bir konteynırın logunu göster\n")
 
 
     #Take user's choice as number
@@ -26,11 +39,12 @@ def showFunctions():
     while (choice != '0'):
         if (choice == '1'):
             return show() 
+        elif (choice == '2'):
+            return show_log()
         else:
             break
 
-        """elif (choice == '2'):
-            return X
+        """
         elif (choice == '2'):
             return X
         
